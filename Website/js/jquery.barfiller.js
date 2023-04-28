bdd85829e837613faa30e57b43fce81d5c9c0d38
pritzdorado <131881073+pritzdorado@users.noreply.github.com>
2023-04-29 06:08:55 +0800

    Update jquery.barfiller.js

--- Website/js/jquery.barfiller.js
+++ Website/js/jquery.barfiller.js
@@ -2,10 +2,6 @@
 * File: jquery.barfiller.js
 * Version: 1.0.1
 * Description: A plugin that fills bars with a percentage you set.
-* Author: 9bit Studios
-* Copyright 2012, 9bit Studios
-* http://www.9bitstudios.com
-* Free to use and abuse under the MIT license.
 * http://www.opensource.org/licenses/mit-license.php
 */
 
